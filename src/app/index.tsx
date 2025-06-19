d8a76dbd8f28b8060d329da3a6ae0c367341dcdb
LakshyaWalia09 <lakshyawalia09@gmail.com>
2025-06-19 19:25:48 +0530

    another update

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,12 @@
-import { Image, View } from "react-native";
-
+import { Link } from "expo-router";
+import { View } from "react-native";
 
 export default function RootLayout() {
   return (
-    // <View className="flex-1 flex-col bg-white dark:bg-black dark:text-cyan-50">
-    //     <Link href="/signup" className="text-white font-bold text-center bg-blue-500 px-6 py-3 rounded-md active:bg-blue-800">
-    //    Go to Signup Page
-    //     </Link>
-    //     <Text className="dark:text-cyan-500">
-    //       Hello
-    //     </Text>
-    // </View>
-    <View>
-      <Image></Image>
-
-   </View>
+    <View className="flex-1 justify-center items-center bg-white">
+        <Link href="/signup" className="text-white font-bold text-center bg-blue-500 px-6 py-3 rounded-md active:bg-blue-800">
+       Go to Signup Page
+        </Link>
+    </View>
   );
 }
