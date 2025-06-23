76b6708b01ee66f74c64108db54a9bd8ab9d8a6a
LakshyaWalia09 <lakshyawalia09@gmail.com>
2025-06-24 00:15:02 +0530

    Start work on a standard overlay (alert) message popup

---
+++ src/components/ui/StandardOverlay.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Modal, Text, View } from 'react-native';
+
+type SOProps = {
+  text: string;
+  Activated: boolean;
+};
+
+
+const StandardOverlay= (props: SOProps) =>{
+  return (
+    <Modal visible={props.Activated}>
+        <View>
+            <Text>StandardOverlay {props.text}</Text>
+        </View>
+    </Modal>
+  )
+}
+
+export default StandardOverlay
