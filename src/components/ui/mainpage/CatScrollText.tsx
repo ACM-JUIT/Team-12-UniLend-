b2ea8150c83c1b336a48c21bd554267b6bc17c35
LakshyaWalia09 <lakshyawalia09@gmail.com>
2025-06-25 21:47:02 +0530

    beta fixes 2.0

--- src/components/ui/mainpage/CatScrollText.tsx
+++ src/components/ui/mainpage/CatScrollText.tsx
@@ -1,48 +1,84 @@
-import React, { useEffect, useState } from 'react';
-import { ScrollView, StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
-import { getActiveTags, tag } from '@/src/api/firestore/tags';
+// import React, { useEffect, useState } from 'react';
+// import { ScrollView, StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
+// import { getActiveTags, tag } from '@/src/api/firestore/tags';
 
-const CatScrollText = () => {
-    const[categories,setCategories] = useState<tag[]>([]);
-    const[selectedId,setSelectedId] = useState<string | null>(null);
+// const CatScrollText = () => {
+//     const[categories,setCategories] = useState<tag[]>([]);
+//     const[selectedId,setSelectedId] = useState<string | null>(null);
     
-  useEffect(() => {
-    const fetchTags = async () => {
-      try {
-        const tags = await getActiveTags();
-        setCategories(tags);
-      } catch (err) {
-        console.error('Error fetching tags:', err);
-      }
-    };
+//   useEffect(() => {
+//     const fetchTags = async () => {
+//       try {
+//         const tags = await getActiveTags();
+//         setCategories(tags);
+//       } catch (err) {
+//         console.error('Error fetching tags:', err);
+//       }
+//     };
 
-    fetchTags();
-  }, []);
+//     fetchTags();
+//   }, []);
 
 
+//   return (
+//     <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+//       <View style={styles.container}>
+//         {categories.map((category) => (
+//           <TouchableWithoutFeedback
+//             key={category.id}
+//             onPress={() => setSelectedId(category.id)}
+//           >
+//             <View
+//               style={
+//                 selectedId === category.id
+//                   ? styles.boxClicked
+//                   : styles.boxUnClick
+//               }
+//             >
+//               <Text>{category.name}</Text>
+//             </View>
+//           </TouchableWithoutFeedback>
+//         ))}
+//       </View>
+//     </ScrollView>
+//   );
+// };
+// export default CatScrollText
+
+
+
+import React, { useState } from 'react';
+import { ScrollView, StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
+const CatScrollText = () => {
+    const categories = [
+  { id: 1, name: "Books" },
+  { id: 2, name: "Calculators" },
+  { id: 3, name: "Electronics" },
+  { id: 4, name: "Accessories" },
+  { id: 5, name: "Lab Equipment" },
+  { id: 6, name: "Room Essentials" },
+  { id: 7, name: "Stationery" },
+  { id: 8, name: "Apparel" },
+  { id: 9, name: "Misc" }
+    ];
   return (
-    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-      <View style={styles.container}>
-        {categories.map((category) => (
-          <TouchableWithoutFeedback
-            key={category.id}
-            onPress={() => setSelectedId(category.id)}
-          >
-            <View
-              style={
-                selectedId === category.id
-                  ? styles.boxClicked
-                  : styles.boxUnClick
-              }
-            >
-              <Text>{category.name}</Text>
-            </View>
-          </TouchableWithoutFeedback>
-        ))}
-      </View>
+    <ScrollView horizontal>
+    <View style={styles.container}>
+        {categories.map((category)=>{
+            const [Clicked, setClicked] = useState(false)
+            return(
+            <TouchableWithoutFeedback onPress={()=>setClicked(!Clicked)} key={category.id}>
+                <Text key={category.id} style={Clicked?styles.boxClicked:styles.boxUnClick}>
+                        {category.name}
+                </Text>
+            </TouchableWithoutFeedback>
+            )
+        })}
+    </View>
     </ScrollView>
-  );
-};
+  )
+}
+
 export default CatScrollText
 
 const styles=StyleSheet.create({
