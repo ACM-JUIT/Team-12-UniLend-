85a325b9c0ed7d1177957e90e6021692b4438cd9
LakshyaWalia09 <lakshyawalia09@gmail.com>
2025-06-26 19:41:03 +0530

    Start work on forum

---
+++ src/components/forms/SubmitBook.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SubmitBook = () => {
+  return (
+    <View>
+      <Text>SubmitBook</Text>
+    </View>
+  )
+}
+
+export default SubmitBook
