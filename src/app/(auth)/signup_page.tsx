da3dfd33a2bbdb8560664a82409a467029f6f0ff
LakshyaWalia09 <lakshyawalia09@gmail.com>
2025-06-27 19:30:41 +0530

    Start work on add listing page

--- src/app/(auth)/signup_page.tsx
+++ src/app/(auth)/signup_page.tsx
@@ -78,7 +78,12 @@ export default function SignUp() {
       return;
     }
     try {
-      await signUp({ username: userName, email: email.toLowerCase(), password, privacyPolicy });
+      await signUp({
+        username: userName,
+        email: email.toLowerCase(),
+        password,
+        privacyPolicy,
+      });
 
       Alert.alert("Account created", "Account successfully created");
       // reset the login page
--- src/app/screens/CreateListing.tsx
+++ src/app/screens/CreateListing.tsx
@@ -1,14 +1,71 @@
 import NavBar from "@/src/components/ui/mainpage/Navbar";
 import React from "react";
-import { ScrollView, StyleSheet, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+} from "react-native";
 
+import {
+  Select,
+  SelectBackdrop,
+  SelectContent,
+  SelectDragIndicator,
+  SelectDragIndicatorWrapper,
+  SelectInput,
+  SelectItem,
+  SelectPortal,
+  SelectTrigger,
+  
+} from "@/components/ui/select";
 const CreateListing = () => {
   return (
-    <View style={styles.container}>
-      <ScrollView style={styles.container}>
-        <NavBar name="Add Listing" />
+    <KeyboardAvoidingView enabled behavior="padding" style={styles.container}>
+      <NavBar name="Add Listing" />
+
+      <ScrollView style={styles.ScrollContainer}>
+        <Text style={styles.heading1}>Item Name*</Text>
+        <TextInput style={styles.input1} placeholder="eg. Lakshya" />
+
+        <Text style={styles.heading1}>Model/Edition*</Text>
+        <TextInput style={styles.input1} placeholder="eg. Manik Edition" />
+        <Select>
+          <SelectTrigger>
+            <SelectInput />
+          </SelectTrigger>
+          <SelectPortal>
+            <SelectBackdrop />
+            <SelectContent>
+              <SelectDragIndicatorWrapper>
+                <SelectDragIndicator />
+              </SelectDragIndicatorWrapper>
+              <SelectItem />
+            </SelectContent>
+          </SelectPortal>
+        </Select>
+
+        <Text style={styles.heading1}>Company/Publication*</Text>
+        <TextInput style={styles.input1} placeholder="eg. Jaypee Cement" />
+
+        <Text style={styles.heading1}>Item Type</Text>
+        <TextInput style={styles.input1} placeholder="eg. Gun" />
+
+        <Text style={styles.heading1}>Item Discription</Text>
+        <TextInput
+          multiline={true}
+          style={styles.input1}
+          placeholder="eg. A platform to sell books that is created by three people oh five people"
+        />
+
+        <Text style={styles.heading1}>Trade Type</Text>
+        <TextInput
+          style={styles.input1}
+          placeholder="eg. Engineering Mathematics"
+        />
       </ScrollView>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -19,7 +76,33 @@ const styles = StyleSheet.create({
     backgroundColor: "#1C161E",
     height: "100%",
     width: "100%",
+    padding: 5,
+  },
+  ScrollContainer: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#1C161E",
+    height: "100%",
+    width: "100%",
+    padding: 5,
+  },
+  input1: {
+    color: "#EFE3C8",
+    borderColor: "#EFE3C8",
     padding: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    backgroundColor: "rgba(245, 245, 220, 0.2)",
+  },
+  heading1: {
+    margin: 5,
+    borderRadius: 10,
+    color: "#ffffff",
+    fontFamily: "Rosarivo",
+    fontSize: 16,
+    marginTop: 10,
+    textShadowRadius: 20,
+    textShadowColor: "#11111192",
   },
 });
 
