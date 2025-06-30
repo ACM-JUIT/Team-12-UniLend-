6a4e8cec8f117419217d956af92939a9754727bc
LakshyaWalia09 <lakshyawalia09@gmail.com>
2025-06-30 23:49:12 +0530

    remove a warn

--- src/app/(frontend)/(products)/[prodId].tsx
+++ src/app/(frontend)/(products)/[prodId].tsx
@@ -1,1 +1,14 @@
 // to open specific book page using bookid
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <View>
+      <Text>
+        
+      </Text>
+    </View>
+  )
+}
