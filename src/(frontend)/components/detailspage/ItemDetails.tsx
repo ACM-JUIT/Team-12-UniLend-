9f9f2b90fb5f41e13aacebee93e75a68c949d9a2
LakshyaWalia09 <lakshyawalia09@gmail.com>
2025-07-04 19:00:11 +0530

    Update product page

--- src/(frontend)/components/detailspage/ItemDetails.tsx
+++ src/(frontend)/components/detailspage/ItemDetails.tsx
@@ -1,12 +1,75 @@
-import { View, Text,Image,TouchableOpacity } from 'react-native'
-import React from 'react'
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
 
-const ItemDetails = () => {
+type detailsProps = {
+  data: any;
+};
+
+const ItemDetails = (props: detailsProps) => {
   return (
-    <View>
-      
+    <View style={styles.container}>
+      <Image style={styles.imgUploaded} source={props.data.img} />
+      <Text numberOfLines={2} style={styles.title}>
+        {props.data.name}
+      </Text>
+      <Text numberOfLines={2} style={styles.pub}>
+        {props.data.publorAuth}
+      </Text>
+      <Text numberOfLines={1} style={styles.itemtag}>
+        {props.data.tag}
+      </Text>
+      <Text style={styles.itemdisc} numberOfLines={3}>
+        {props.data.disc}
+      </Text>
     </View>
-  )
-}
+  );
+};
 
-export default ItemDetails
+export default ItemDetails;
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+  },
+  imgUploaded: {
+    height: 318,
+    width: 207,
+    borderRadius: 30,
+    borderWidth: 1.5,
+    borderColor: "black",
+  },
+  title: {
+    fontSize: 24,
+    fontFamily: "Amiri",
+    textAlign: "center",
+    lineHeight: 25,
+    color: "#F5F5DC",
+    textTransform: "capitalize",
+  },
+  pub: {
+    textTransform: "uppercase",
+    fontFamily: "Rosarivo",
+    fontSize: 16,
+    color: "#F5F5DC",
+    lineHeight: 16,
+  },
+  itemtag: {
+    fontFamily: "Rosarivo",
+    fontSize: 12,
+    textTransform: "capitalize",
+    lineHeight: 12,
+    color: "#FFFFFF",
+  },
+  itemdisc: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: "#FFFFFF",
+    textOverflow: "",
+    paddingLeft: 10,
+    paddingRight:10
+  },
+});
--- src/app/(screens)/[prodId].tsx
+++ src/app/(screens)/[prodId].tsx
@@ -1,25 +1,42 @@
 // to open specific book page using bookid
+import ItemDetails from "@/src/(frontend)/components/detailspage/ItemDetails";
 import TopActions from "@/src/(frontend)/components/detailspage/TopActions";
-import NavBar from "@/src/(frontend)/components/standard/Navbar";
 import StandardOverlay from "@/src/(frontend)/components/standard/StandardOverlay";
 import { router } from "expo-router";
 import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-type data = {
+import UploadedImage from "../../../assets/images/harry-potter.png";
+type dataInfo = {
   id: number;
   name: string;
+  publorAuth: string;
+  disc: string;
+  tag: string;
+  type: string;
+  img: any;
 };
-const data = [
-  { id: 1, name: "test1" },
-  { id: 2, name: "test2" },
-];
+const dataInfo = {
+  id: 1,
+  name: "Harry Potter and the phlosopher's stone",
+  disc: "It follows Harry Potter, a young wizard Lakshya who discovers his magical heritage on his eleventh birthday when he receives a letter of acce Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio, esse cumque, ipsam ex similique quisquam molestias dignissimos sapiente quae eos animi repudiandae vero blanditiis id fuga, ipsa pariatur ratione exercitationem.",
+  tag: "Books/Novels",
+  type: "Sell",
+  img: UploadedImage,
+  publorAuth: "JK Rowling",
+};
 const [SOopen, setSOopen] = useState(false);
 export default function () {
   return (
     <SafeAreaView style={styles.container}>
-      <NavBar title="Like It?" />
-      <TopActions backFunc={router.back} itemId={data} watchBut={setSOopen} />
+      {/* <NavBar title="Like It?" /> */}
+      <TopActions
+        backFunc={router.back}
+        itemId={dataInfo}
+        watchBut={setSOopen}
+      />
+      <ItemDetails data={dataInfo} />
+
       <StandardOverlay
         Activated={SOopen}
         Controller={setSOopen}
