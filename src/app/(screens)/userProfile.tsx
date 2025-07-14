2453ddc1279db103001867ed047d54e1e1ad5127
lakshay1403 <rudrakshg16@gmail.com>
2025-07-14 16:37:05 +0530

    user profile backend

--- src/app/(screens)/userProfile.tsx
+++ src/app/(screens)/userProfile.tsx
@@ -3,32 +3,60 @@ import NavBar from "@/src/(frontend)/components/standard/Navbar";
 import Browse from "@/src/(frontend)/components/userprofile/Browse";
 import ProfileImg from "@/src/(frontend)/components/userprofile/pfp";
 import UserInfo from "@/src/(frontend)/components/userprofile/userinfo";
-import React from "react";
+import React,{useEffect,useState} from "react";
 import { StyleSheet, View } from "react-native";
+import { getUserProfile } from "@/src/api/firestore/user";
+import { getAuth } from "@react-native-firebase/auth";
 
-let datatest = {
-  name: "Lakshya Walia",
-  username: "lakshyawalia01",
-  sales: 4,
-  hostel: "H10-R4",
-  mobile: "+46465455X",
-  email: "lakshya2332@juitsolan.in",
-};
+export default function UserProfile() {
+  const user = getAuth().currentUser;
 
-const userProfile = () => {
+  const [profile,setProfile] = useState({
+    name: "",
+    username: "",
+    sales: 0,
+    hostel: "",
+    mobile: "",
+    email: "",
+    photoURL: "",
+  });
+
+  const [loading,setLoading] = useState(true);
+
+    useEffect(() => {
+      const fetchprofile = async () => {
+        if(!user) return;
+        try{
+          const data = await getUserProfile(user.uid);
+          setProfile({
+          name: typeof data.name === "string" ? data.name : "",
+          username: typeof data.username === "string" ? data.username : "",
+          sales: typeof data.sales === "number" ? data.sales : 0,
+          hostel: typeof data.hostel === "string" ? data.hostel : "",
+          mobile: typeof data.mobile === "string" ? data.mobile : "",
+          email: typeof data.email === "string" ? data.email : "",
+          photoURL: typeof data.photoURL === "string" ? data.photoURL : "",
+          });
+        } catch (error) {
+          console.error("Failed to fetch profile:",error);
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      fetchprofile();
+    });
   return (
     <View style={styles.container}>
-      <NavBar title={datatest.name} />
+      <NavBar title={profile.name || "Your profile"} />
       <BackButton />
       {/* Use the image prop below to push the user image, it has a default image */}
-      <ProfileImg />
-      <UserInfo data={datatest} />
+      <ProfileImg image = {profile.photoURL}/>
+      <UserInfo data={profile} />
       <Browse />
     </View>
   );
-};
-
-export default userProfile;
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -39,3 +67,12 @@ const styles = StyleSheet.create({
     gap: 10,
   },
 });
+
+let datatest = {
+  name: "Lakshya Walia",
+  username: "lakshyawalia01",
+  sales: 4,
+  hostel: "H10-R4",
+  mobile: "+46465455X",
+  email: "lakshya2332@juitsolan.in",
+};
