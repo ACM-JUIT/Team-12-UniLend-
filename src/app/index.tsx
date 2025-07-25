3d9f768190247199f1470c89c29b6d82f250b3da
Manik2375 <manikkpr2375@gmail.com>
2025-07-25 14:05:27 +0530

    fix: add redirect in onBoarding page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,11 @@
+import { getAuth } from "@react-native-firebase/auth";
+import { Redirect } from "expo-router";
 import OnBoarding from "./(auth)/onBoarding";
 
 export default function RootLayout() {
+  const user = getAuth().currentUser;
+  if (user) {
+    return <Redirect href="/(screens)/homeScreen" />
+  }
   return <OnBoarding />;
 }
